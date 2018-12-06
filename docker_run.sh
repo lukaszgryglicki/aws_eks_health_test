55ace6fc7122a7e84e1bb5ac75c64c32d8cab137
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-06 13:39:09 +0000

    Fixes
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker_run.sh
+++ docker_run.sh
@@ -4,13 +4,24 @@ then
   echo "usage: ${0} docker_username"
   exit 1
 fi
-host=`docker run "${1}/ekshealthtest" ip route show | awk '/default/ {print $3}'`
+if [ -z "${EKS_HEALTH_PORT}" ]
+then
+  EKS_HEALTH_PORT=8888
+fi
 if [ -z "${PG_HOST}" ]
 then
+  if [ -z "${host}" ]
+  then
+    host=`docker run "${1}/ekshealthtest" ip route show | awk '/default/ {print $3}'`
+  fi
   export PG_HOST="${host}"
 fi
 if [ -z "${ES_HOST}" ]
 then
+  if [ -z "${host}" ]
+  then
+    host=`docker run "${1}/ekshealthtest" ip route show | awk '/default/ {print $3}'`
+  fi
   export ES_HOST="${host}"
 fi
 docker run -it -p "${EKS_HEALTH_PORT}:${EKS_HEALTH_PORT}" -e SKIP_HTTP="${SKIP_HTTP}" -e EKS_HEALTH_PORT="${EKS_HEALTH_PORT}" --env-file <(env | grep PG_) --env-file <(env | grep ES_) "${1}/ekshealthtest"
